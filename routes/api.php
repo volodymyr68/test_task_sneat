b767085978351e090b0dd4fbc32cf2f111b4b68b
v-senchenko <v.senchenko@crmium.com>
2025-03-21 12:19:36 +0200

    hotfix2

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,18 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
 
-    Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::get('logout', [AuthController::class, 'logout']);
         Route::get('user', [AuthController::class, 'user']);
         Route::post('check', [AuthController::class, 'checkAuth']);
-        Route::apiResource('posts', PostController::class);
     });
 });
-
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('posts', PostController::class);
+});
